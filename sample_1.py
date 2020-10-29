59a25f63c86c3d83012c4eda523a723c95e46a92
root <ronaldocr9803@gmail.com>
2020-10-29 17:45:36 +0000

    a

--- sample_1.py
+++ sample_1.py
@@ -120,9 +120,11 @@ def get_prediction(model, img_path, threshold):
     pred_class = [CLASS_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
     pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
     pred_score = list(pred[0]['scores'].detach().numpy())
+    # print(pred_score)
     # import ipdb; ipdb.set_trace()
     pred_t = [pred_score.index(x) for x in pred_score if x > threshold] # Get list of index with score greater than threshold.
-    if pred_t is None:
+    print(pred_t)
+    if not pred_t:
         return [], [], []
     else:
         pred_t = pred_t[-1]
